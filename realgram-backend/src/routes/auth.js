66428d7710adb2165b797bb957b92579ca7d0f81
Gabriel Reis <username.gran@gmail.com>
2023-07-22 16:31:06 -0300

    Criada as rotas de postagens e alterado o arquivo auth.js, em routes

--- realgram-backend/src/routes/auth.js
+++ realgram-backend/src/routes/auth.js
@@ -9,18 +9,45 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const requireLogin = require('../middleware/requireLogin')
 
+//middleware para verificação de token
 router.get('/protected', requireLogin, (req, res) => {
   res.send("Hello user")
 })
 
+//cadastro de usuário
 router.post('/signup', (req, res) => {
   const { name, email, username, password } = req.body
   if (!name || !email || !username || !password) {
     return res.status(422).json({ error: "Por favor, preencha todos os campos." })
   }
+
+//Erro ao encontrar username já cadastrado
   User.findOne({ username: username }).then((savedUser) => {
     if (savedUser) {
-      return res.status(422).json({ error: "Nome de usuário já cadastrado." })
+      return res.status(409).json({ error: "Nome de usuário já cadastrado." })
+    }
+    bcrypt.hash(password, 12).then(hashedpassword => {
+      const user = new User({
+        name,
+        email,
+        username,
+        password: hashedpassword
+      })
+      user.save().then(user => {
+        res.json({ message: "Salvo com sucesso!" })
+      }).catch(err => {
+        console.log(err)
+      })
+    })
+
+  }).catch(err => {
+    console.log(err)
+  })
+
+//Erro ao encontrar email já cadastrado
+  User.findOne({ email: email }).then((savedUser) => {
+    if (savedUser) {
+      return res.status(409).json({ error: "Email já cadastrado." })
     }
     bcrypt.hash(password, 12).then(hashedpassword => {
       const user = new User({
@@ -41,6 +68,19 @@ router.post('/signup', (req, res) => {
   })
 })
 
+//Listar usuário
+router.get('/users', (req, res) => {
+  User.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Ocorreu um erro ao buscar os usuários." });
+    });
+});
+
+//login
 router.post('/login', (req, res) => {
   const { email, password } = req.body
   if (!email || !password) {
@@ -52,7 +92,6 @@ router.post('/login', (req, res) => {
     }
     bcrypt.compare(password, savedUser.password).then(doMatch => {
       if (doMatch) {
-        //res.json({ message: "Login realizado com sucesso" })
         const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET)
         res.json({ token })
       } else {
@@ -63,5 +102,39 @@ router.post('/login', (req, res) => {
     })
   })
 })
+
+// Encontrar usuário pelo nome de usuário (username)
+router.get('/users/:username', (req, res) => {
+  const username = req.params.username;
+
+  User.findOne({ username: username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado." });
+      }
+      res.json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Ocorreu um erro ao buscar o usuário." });
+    });
+});
+
+//remover usuário pelo id
+router.delete('/users/:userId', (req, res) => {
+  const userId = req.params.userId;
+
+  User.findByIdAndRemove(userId)
+    .then(deletedUser => {
+      if (!deletedUser) {
+        return res.status(404).json({ error: "Usuário não encontrado." });
+      }
+      res.json({ message: "Usuário removido com sucesso." });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Ocorreu um erro ao remover o usuário." });
+    });
+});
 
 module.exports = router
--- realgram-backend/src/server.js
+++ realgram-backend/src/server.js
@@ -1,29 +1,30 @@
 require('dotenv').config()
 const MONGOURI = process.env.MONGOURI
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const port = 3000;
 
-require("./models/user")
-
+require('./models/user')
+require('./models/post')
 app.use(express.json())
 app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
 
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 })
 
 mongoose.connection.on('connected', () => {
-  console.log("Conectado ao Mongo")
+  console.log('Conectado ao Mongo')
 })
 mongoose.connection.on('err', (err) => {
-  console.log("Erro ao conectar o Mongo", err)
+  console.log('Erro ao conectar o Mongo', err)
 })
 
 app.listen(port, () => {
   console.log(`####### REALGRAM API #######`);
   console.log(`Servidor iniciado na porta: ${port}`);
-});
+})
