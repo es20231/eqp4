c2ad477764693e8552a30052ed7c1fd09ede320c
vicenttcarvalho <vicentecleyton@gmail.com>
2023-07-22 10:54:40 -0300

    middleware para verificar token

--- realgram-backend/src/keys.js
+++ realgram-backend/src/keys.js
@@ -1,3 +1,4 @@
-module.exports={
-  MONGOURI:"mongodb+srv://realgram:HD2p9wdgrbSpb6wl@google-cloud-sp-realgra.4le4qef.mongodb.net/?retryWrites=true&w=majority"
+module.exports = {
+  MONGOURI: "mongodb+srv://realgram:HD2p9wdgrbSpb6wl@google-cloud-sp-realgra.4le4qef.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "eghuageuas23ha"
 }
