0ca51a65b12fc29947ffbb96be34b98fc818e5ba
pumba-dev <pumbadeveloper@gmail.com>
2023-08-21 11:39:01 -0300

    Fix: Uploads directory path change

--- realgram-backend/src/routes/library.js
+++ realgram-backend/src/routes/library.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const User = mongoose.model("User");
 const Library = require("../models/library.js");
-const multer = require('multer');
-const requireLogin = require('../middleware/requireLogin.js');
+const multer = require("multer");
+const requireLogin = require("../middleware/requireLogin.js");
 
 // Configurando o armazenamento do multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/user_images');
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 // Filtro para permitir apenas imagens
 const imageFilter = (req, file, cb) => {
   if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-    return cb(('Somente imagens .jpg, .jpeg e .png são permitidas!'), false);
+    return cb("Somente imagens .jpg, .jpeg e .png são permitidas!", false);
   }
   cb(null, true);
 };
@@ -29,38 +29,45 @@ const imageFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   limits: { fileSize: 10 * 1024 * 1024 }, // Limite de 10MB para o tamanho do arquivo
-  fileFilter: imageFilter
+  fileFilter: imageFilter,
 });
 
 // Rota para upload de ate 10 imagens por envio
-router.post('/library/save-image-list', requireLogin, (req, res, next) => {
-  upload.array('images', 10)(req, res, (err) => {
-    if (err) {
-      if (err.code === 'LIMIT_UNEXPECTED_FILE') {
-        return res.status(400).json({ error: 'O limite por envio são de 10 imagens.' });
+router.post(
+  "/library/save-image-list",
+  requireLogin,
+  (req, res, next) => {
+    upload.array("images", 10)(req, res, (err) => {
+      if (err) {
+        if (err.code === "LIMIT_UNEXPECTED_FILE") {
+          return res
+            .status(400)
+            .json({ error: "O limite por envio são de 10 imagens." });
+        }
+      } else {
+        next();
       }
-    } else {
-      next();
+    });
+  },
+  async (req, res) => {
+    const userId = req.user.id; // ID do usuário autenticado
+
+    try {
+      const uploadedImages = req.files.map((file) => ({
+        fileName: file.filename,
+        uploadedBy: new mongoose.Types.ObjectId(userId),
+      }));
+
+      await UserImage.create(uploadedImages);
+
+      res.status(200).json({ message: "Salvo com sucesso!" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Ocorreu um erro ao enviar as imagens." });
     }
-  });
-}, async (req, res) => {
-  const userId = req.user.id; // ID do usuário autenticado
-
-  try {
-    const uploadedImages = req.files.map(file => ({
-      fileName: file.filename,
-      uploadedBy: new mongoose.Types.ObjectId(userId)
-    }));
-
-    await UserImage.create(uploadedImages);
-
-    res.status(200).json({ message: 'Salvo com sucesso!' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Ocorreu um erro ao enviar as imagens.' });
   }
-});
-  
+);
+
 // Rota para salvar uma imagem
 router.post(
   "/library/save-image",
@@ -87,17 +94,17 @@ router.post(
       return res
         .status(500)
         .json({ error: "Erro ao salvar a imagem no banco de dados" });
-      }
+    }
   }
 );
 
 //rota GET para recuperar informações sobre as imagens associadas a um usuário
-router.get('/library/images/:userId', requireLogin, async (req, res) => {
+router.get("/library/images/:userId", requireLogin, async (req, res) => {
   const userId = req.params.userId;
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: 'Usuário não encontrado.' });
+      return res.status(404).json({ error: "Usuário não encontrado." });
     }
     // Encontre as imagens associadas ao usuário pelo ID do usuário
     const userImages = await UserImage.find({ uploadedBy: userId });
@@ -105,35 +112,34 @@ router.get('/library/images/:userId', requireLogin, async (req, res) => {
     res.status(200).json({ images: userImages });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Ocorreu um erro ao buscar as imagens.' });
+    res.status(500).json({ error: "Ocorreu um erro ao buscar as imagens." });
   }
 });
 
-router.delete('/library/remove-image/:id', requireLogin, async (req, res) => {
+router.delete("/library/remove-image/:id", requireLogin, async (req, res) => {
   try {
     const imageId = req.params.id;
     const existingImage = await UserImage.findById(imageId);
 
     if (!existingImage) {
-      return res.status(404).json({ error: 'Imagem não encontrada.' });
+      return res.status(404).json({ error: "Imagem não encontrada." });
     }
 
     await UserImage.findByIdAndRemove(imageId);
 
-    const imagePath = path.join('./public/user_images', existingImage.fileName);
+    const imagePath = path.join("./uploads", existingImage.fileName);
 
     // Verificar se o arquivo existe antes de tentar excluí-lo
     if (fs.existsSync(imagePath)) {
       fs.unlinkSync(imagePath);
-      res.status(200).json({ message: 'Imagem removida com sucesso!' });
+      res.status(200).json({ message: "Imagem removida com sucesso!" });
     } else {
-      res.status(404).json({ error: 'Arquivo de imagem não encontrado.' });
+      res.status(404).json({ error: "Arquivo de imagem não encontrado." });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Ocorreu um erro ao remover a imagem.' });
+    res.status(500).json({ error: "Ocorreu um erro ao remover a imagem." });
   }
 });
-
 
 module.exports = router;
